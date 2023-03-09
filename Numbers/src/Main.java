bbf8cda80d6ed77685f4147832c66fdbc3253da5
Hababoks <126653941+Hababoks@users.noreply.github.com>
2023-03-09 21:22:10 +0200

    Add files via upload
    “Initial commit”

---
+++ Numbers/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void
+    main(String[] args) {
+        System.out.printf("%.2f" , 3.222);
+        System.out.printf(" ");
+        System.out.printf("%.2f", 4.333);
+        System.out.printf(" ");
+        System.out.printf("%.2f", 5.444);
+    }
+}
---
+++ People/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Viktor Ivanovich Melnik, Age=25y.o., Height=173cm.");
+        System.out.println();
+        System.out.println("Abramov Sergey Andreevich, Age=47y.o., Height=188cm.");
+        System.out.println();
+        System.out.println("Pushkin Sergey Leonidovich, Age 18y.o., Height=191cm.");
+    }
+}
